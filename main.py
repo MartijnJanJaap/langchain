6eab83f8a94f16e7a8f7ff97af4f5809829fea0f
anonymous <a@a.com>
2025-04-03 13:58:58 +0200

    wip getting started langgraph

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ from langgraph.graph import StateGraph
 
 from load_api_key_from_file import load_api_key_from_file
 
-def main():
-    api_key = load_api_key_from_file()
+api_key = load_api_key_from_file()
 
 
 class State(TypedDict):
@@ -45,12 +44,47 @@ builder.add_edge("node_3", END)
 
 graph = builder.compile()
 
-img_data = graph.get_graph().draw_mermaid_png()
-img = Image.open(BytesIO(img_data))
-img.show()
+# visualize:
+# img_data = graph.get_graph().draw_mermaid_png()
+# img = Image.open(BytesIO(img_data))
+# img.show()
 
 final_state = graph.invoke({"graph_state": "Hi, this is martijn"})
 print(final_state)
 
-if __name__ == "__main__":
-    main()
+
+
+
+
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+
+messages = [AIMessage(content=f"So you said you were researching ocean mammals?", name="Model")]
+messages.extend([HumanMessage(content=f"Yes thats right.", name="Martijn")])
+messages.extend([AIMessage(content=f"Great, what would you like to learn about them?.", name="Model")])
+messages.extend([HumanMessage(content=f"I want to learn about the best place to see Orcas in the US.", name="Martijn")])
+
+for m in messages:
+    m.pretty_print()
+
+
+
+from langchain_openai import ChatOpenAI
+llm = ChatOpenAI(model = "gpt-4o", api_key= api_key)
+result = llm.invoke(messages)
+print(type(result))
+print(result)
+
+
+
+def multiply(a: int, b: int) -> int:
+    """Multiply a nd b.
+
+    Args:
+     a: first int
+    b: second int
+    """
+    return a * b
+
+llm_with_tools = llm.bind_tools([multiply])
+tool_call = llm_with_tools.invoke([HumanMessage(content = f" what is 3 multiplied by 20 ", name="Martijn")])
+print(tool_call)
